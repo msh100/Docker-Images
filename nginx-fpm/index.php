253df17f41d6ca20047e7a8f85f0e89917fd30e3
Marcus Hughes <hello@entirely.pro>
2014-07-03 11:15:40 -0400

    Nginx and PHP-FPM

---
+++ nginx-fpm/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Nginx and PHP-FPM installed</title>
+    </head>
+    
+    <body>
+        <h1>PHP-FPM and Nginx are installed</h1>
+        To proceed, you should add your own contents to the /app/ directory as the user `app`.
+        <br/> <br/>
+        <?php phpinfo(); ?>
+    </body>
+</html>
